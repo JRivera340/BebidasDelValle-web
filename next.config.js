3c16a67cbc93188982fdba6d6ed425ffba765d02
Joshua <riveragonzalezjoshua404@gmail.com>
2025-10-28 13:47:38 -0500

    feat: Actualizar imÃ¡genes con placeholders profesionales usando colores de marca

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
